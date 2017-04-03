ca606355d8dbc7eedd2f500de44f0f56c52724dc
Owner <Owner@DESKTOP-T042BF3>
2017-04-02 20:01:42 -0400

    Add project files.

---
+++ briggs_Reviews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="briggs_Reviews.MvcApplication" Language="C#" %>
---
+++ briggs_Reviews/Migrations/Configuration.cs
@@ -0,0 +1,88 @@
+namespace briggs_Reviews.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<briggs_Reviews.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(briggs_Reviews.Models.ApplicationDbContext context)
+        {
+            context.Movies.AddOrUpdate(b => b.Title,
+                 new Movie
+                 {
+                     ID = 1,
+                     Title = "American Sniper",
+                     Year = 2014,
+                     Category = "Action",
+                     LeadingActor = "Bradley Cooper",
+                     Director = "Clint Eastwood",
+                     Rating = MovieRating.D
+
+                 },
+                  new Movie
+                  {
+                      ID = 2,
+                      Title = "The Princess Bride",
+                      Year = 1987,
+                      Category = "Fantasy",
+                      LeadingActor = "Cary Elwes",
+                      Director = "Rob Reiner",
+                      Rating = MovieRating.C,
+                  },
+                  new Movie
+                  {
+                      ID = 3,
+                      Title = "10 Things I Hate About You",
+                      Year = 1999,
+                      Category = "Comedy",
+                      LeadingActor = "Heath Ledger",
+                      Director = "Gil Junger",
+                      Rating = MovieRating.A
+                  },
+                  new Movie
+                  {
+                      ID = 4,
+                      Title = "The Little Mermaid",
+                      Year = 1989,
+                      Category = "Animation",
+                      LeadingActor = "Jodi Benson",
+                      Director = "Ron Clements",
+                      Rating = MovieRating.B
+
+                  },
+                   new Movie
+                   {
+                       ID = 5,
+                       Title = "Hidden Figures",
+                       Year = 2016,
+                       Category = "Biography",
+                       LeadingActor = "Teraji P. Henson",
+                       Director = "Theodore Melfi",
+                       Rating = MovieRating.A
+                   }
+                 );
+            context.Reviews.AddOrUpdate(b => b.MovieTitle,
+            new Review
+            {
+                MovieTitle = "Hidden Figures",
+                Content = "I absolutely loved this movie! Very empowering for women in STEM fields!",
+                Rating = MovieRating.A
+            },
+            new Review
+            {
+                MovieTitle = "American Sniper",
+                Content = "Honestly, not my favorite movie. IT was too intense for me to watch",
+                Rating = MovieRating.C
+            }
+            );
+        }
+    }
+}
