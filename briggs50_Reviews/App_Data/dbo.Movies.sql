3c42fd2f23b0655860abf04162fb4ab6d31555d1
Alexandra <Owner@DESKTOP-T042BF3>
2017-04-02 20:56:38 -0400

    Fixed DB

---
+++ briggs50_Reviews/App_Data/dbo.Movies.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Movies] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Title]        NVARCHAR (MAX) NOT NULL,
+    [Year]         INT            NOT NULL,
+    [Category]     NVARCHAR (MAX) NOT NULL,
+    [LeadingActor] NVARCHAR (MAX) NOT NULL,
+    [Director]     NVARCHAR (MAX) NOT NULL,
+    [MoviePoster]  NVARCHAR (MAX) NULL,
+    [Rating]       INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Movies] PRIMARY KEY CLUSTERED ([Id] ASC),
+	 CONSTRAINT [FK_dbo.Movies_Rating] FOREIGN KEY ([Rating]) REFERENCES [dbo].[Reviews] ([Rating]) ON DELETE CASCADE
+);
+
---
+++ briggs50_Reviews/App_Data/dbo.Reviews.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Reviews] (
+    [ID]         INT            IDENTITY (1, 1) NOT NULL,
+    [Content]    NVARCHAR (MAX) NULL,
+    [MovieID]    INT            NOT NULL,
+    [MovieTitle] NVARCHAR (MAX) NULL,
+    [Rating]     INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Reviews] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_MovieID]
+    ON [dbo].[Reviews]([MovieID] ASC);
+
